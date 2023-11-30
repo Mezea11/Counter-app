1c3b56e42aa7cd9f9e9f89002222d44755114c30
Mezea11 <christiancastellanosmeza@gmail.com>
2023-11-30 22:54:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+// get element id
+// set count to 0
+// make a  function (increment) to make count increase in the html when clicked
+
+
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let resetEl = document.getElementById("reset-el")
+
+let count = 0;
+
+
+//FUNCTION THAT INCREMENTS NUMBER
+function increment() {
+    count += 1;
+    countEl.textContent= count;
+}
+
+//SAVE BUTTON
+function save() {
+    let countStr = count + 0;
+    saveEl.textContent += countStr + " / ";
+    countEl.textContent = 0;
+    count = 0;
+}
+
+function reset() {
+    location.reload();
+}
